ca0fe9ec0ddb925e60de768f9e2fbc3f2452d85d
Paul <paul@devpaul.com>
2012-12-01 22:01:46 -0800

    fixing requireSrc to actually require

--- test/controller/test-requireSrc.js
+++ test/controller/test-requireSrc.js
@@ -3,8 +3,20 @@ var unit = requireUnit(__filename)
   , TestGroup = testutil.TestGroup
   , testGroup = new TestGroup()
 
+exports['is constructed properly'] = function(test) {
+    test.ok(unit)
+    test.ok(unit._resolve)
+    test.done()
+}
 exports['maps to source folder'] = function(test) {
     var filename = 'pants.js'
-    test.equal(unit(filename), path.join(path.resolve('lib'), filename))
+    test.equal(unit._resolve(filename), path.join(path.resolve('lib'), filename))
+    test.done()
+}
+
+exports['requireSrc'] = function(test) {
+    var filename = 'controller/requireSrc.js'
+      , testGroup = new TestGroup()
+    test.equal(unit, unit(filename, testGroup))
     test.done()
 }
